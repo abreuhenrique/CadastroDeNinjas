d7a6b46420a3162a457fd5ebc260883cdc01b498
Henrique Abreu <abreuh873@gmail.com>
2026-06-17 15:05:54 -0300

    fix: Adiciona o @Column na Classe NinjaModel

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -16,13 +16,16 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "idade")
     private int idade;
 
     @ManyToOne // Muitos Ninjas/1 Ninja podem ter 1 Missão
