6fb8242b7f5d3c7762965b17e0d7a4621b087982
Henrique Abreu <abreuh873@gmail.com>
2026-06-20 16:41:50 -0300

    feat: Cria os Endpoints Ninja

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,36 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Hello, World!";
+    }
+
+    //Criar Ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+
+    //Mostrar todos os Ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarNinjas() {
+        return "Mostrar Ninjas";
+    }
+
+    //Mostrar Ninja por Id (UPDATE)
+    @GetMapping("/todosID")
+    public String mostrarNinjasId() {
+        return "Mostrar Ninja por ID";
+    }
+
+    //Alterar dados do Ninja (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinja() {
+        return "Alterar um Ninja por Id";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletar")
+    public String deletarNinjaId() {
+        return "Deletar Ninja por ID";
     }
 
 }
