946a734002e8d8a72b0125bc6a97775a0643b913
Henrique Abreu <abreuh873@gmail.com>
2026-06-25 18:23:29 -0300

    fix: Inclui a Coluna de rank na Entidade, porem já estava Incluída na migration V2

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -28,6 +28,9 @@ public class NinjaModel {
     @Column(name = "idade")
     private int idade;
 
+    @Column(name = "rank")
+    private String rank;
+
     @ManyToOne // Muitos Ninjas/1 Ninja podem ter 1 Missão
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
